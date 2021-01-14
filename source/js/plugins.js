5eb24f6eb69e2dd15a024a872544be93d02c02de
rtsarkov <71775043+rtsarkov@users.noreply.github.com>
2021-01-14 17:58:26 +0300

    Add files via upload

---
+++ source/js/plugins.js
@@ -0,0 +1,6 @@
+//=include ../../bower_components/jquery/dist/jquery.js
+//=include ../../bower_components/slick-carousel/slick/slick.min.js
+//=include ../../bower_components/jquery-colorbox/jquery.colorbox.js
+//=include ../../bower_components/jquery.inputmask/dist/jquery.inputmask.bundle.js
+//=include ../../bower_components/jquery.form-styler/dist/jquery.formstyler.min.js
+//=include ../../bower_components/air-datepicker/dist/js/datepicker.min.js
